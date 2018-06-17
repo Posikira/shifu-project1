73fbc6cbdb49a78f9f9a01159619f797463daf6f
Евгений Посикира <posikira1@icloud.com>
2018-06-17 16:00:34 +0300

    Modified the folders for MVC. Added navigation controller for click transition and return. Added TableView datasource.

---
+++ project1/Controller/TableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Eugene Posikyra on 15.06.2018.
+//  Copyright © 2018 Eugene Posikyra. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    var groceriesArray = ["Milk", "Soda", "Apple juice", "Bananas", "Cereals", "Potatoes", "Ginger", "Cat food", "Sausages"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK - TableVeiw Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groceriesArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell", for: indexPath)
+        cell.textLabel?.text = groceriesArray[indexPath.row]
+        return cell
+    }
+    
+    // MARK - TableView delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "anotherSegue", sender: self)
+    }
+
+
+}
+
