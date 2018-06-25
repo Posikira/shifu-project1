99d61dc27e270a15ca74eac9cde2125611b3b8b6
Евгений Посикира <posikira1@icloud.com>
2018-06-25 15:28:32 +0300

    Sending pod files.

---
+++ project1/Model/ListItem.swift
@@ -0,0 +1,16 @@
+//
+//  ListItem.swift
+//  project1
+//
+//  Created by Eugene Posikyra on 19.06.2018.
+//  Copyright © 2018 Eugene Posikyra. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class ListItem: Object {
+    
+    @objc dynamic var text = ""
+    
+}
